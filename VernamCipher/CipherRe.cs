9b6a109899309bd9c1cdf3ea73d47a357a6b9b8e
staric1097 <matveev.sds@yandex.ru>
2023-01-11 17:27:00 +0300

    Add project files.

---
+++ VernamCipher/CipherRe.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace VernamCipher
+{
+    static internal class CipherRe
+    {
+        private static string StringToBits(string text)
+        {
+            //create arrays of bytes
+            byte[] textbyte = Encoding.ASCII.GetBytes(text);
+            string textbitSt = "";
+
+            //create string of bits
+            for (int i = 0; i < textbyte.Length; i++)
+            {
+                string ch = Convert.ToString(textbyte[i],2);
+
+                //insert "0" while char representation length < 8 symbols
+                while (ch.Length < 8)
+                {
+                    ch = ch.Insert(0, "0");
+                }
+                textbitSt = textbitSt.Insert(textbitSt.Length, ch);
+            }
+
+            return textbitSt;
+        }
+
+        public static string Encrypt(string FileName, out string key)
+        {
+            string text = File.ReadAllText(FileName);
+
+            //генерируем ключ нужной длинны
+            key = GenerateKey(text.Length);
+
+            //преобразование текста и ключа в двоичную форму представления
+            text = StringToBits(text);
+            string keybits = StringToBits(key);
+
+            string result="";
+            for (int i = 0; i < text.Length; i++)
+            {
+                bool temp = ToBoolWord(text.Substring(i, 1)) ^ ToBoolWord(keybits.Substring(i, 1));
+                result = result.Insert(result.Length,ToBoolNum(temp));
+            }
+            return result;
+        }
+
+        public static string Decrypt(string datePath,string keyPath)
+        {
+            string encryptedText = File.ReadAllText(datePath);
+            string key = File.ReadAllText(keyPath);
+
+            //преобразование ключа в двоичную систему
+            key = StringToBits(key);
+
+            string result = "";
+            for (int i = 0; i < encryptedText.Length; i++)
+            {
+                bool temp = ToBoolWord(encryptedText.Substring(i, 1)) ^ ToBoolWord(key.Substring(i, 1));
+                result = result.Insert(result.Length, ToBoolNum(temp));
+            }
+            //объявляем byte[]
+            byte[] bytes = new byte[result.Length / 8];
+            int count = 0;
+            for (int i = 0; i < result.Length; i+=8)
+            {
+                //считываем 8 бит в строку
+                string temp = result.Substring(i, 8);
+                //переделываем строку в byte[]
+                bytes[count] = Convert.ToByte(temp, 2);
+                count++;
+            }
+            //переводим в символ
+            result=Encoding.ASCII.GetString(bytes);
+            return result;
+        }
+
+        private static string GenerateKey(int size)
+        {
+            Random random = new Random();
+            string result = "";
+            byte[] temp=new byte[size];
+            for (int i = 0; i < size; i++)
+            {
+                temp[i]=(byte)random.Next(65, 91);
+            }
+            result = Encoding.ASCII.GetString(temp);
+;
+            return result;
+        }
+
+        private static bool ToBoolWord(string s)
+        {
+            if (s == "0") return false;
+            else if (s == "1") return true;
+            else throw new Exception("error ToBoolWord func");
+        }
+        private static string ToBoolNum(bool b)
+        {
+            if (b == false) return "0";
+            else if (b == true) return "1";
+            else throw new Exception("error ToBoolNum func");
+        }
+    }
+}
---
+++ VernamCipher/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace VernamCipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Выберете файл для шифрования", "", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string fileName = openFileDialog1.FileName;
+                if (Path.GetExtension(fileName) == ".txt")
+                {
+                    string key;
+                    string encryptedText=CipherRe.Encrypt(fileName, out key);
+
+                    //сохраним результат
+                    MessageBox.Show("Шифрование выполнено. Сохраните файл с зашифрованными данными...", "Save",MessageBoxButtons.OK ,MessageBoxIcon.Asterisk);
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "Text files(*.txt)|*.txt";
+                    DialogResult dialogResult = saveFileDialog.ShowDialog();
+                    if (dialogResult == DialogResult.OK)
+                    {
+                        string EnT_fileName = saveFileDialog.FileName;
+                        File.WriteAllText(EnT_fileName, encryptedText);
+                    }
+                    else { return; }
+                    MessageBox.Show("Сохраните сохраните ключ...", "Save", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    dialogResult = saveFileDialog.ShowDialog();
+                    if (dialogResult==DialogResult.OK)
+                    {
+                        string key_fileName = saveFileDialog.FileName;
+                        File.WriteAllText(key_fileName, key);
+                    }
+                    else { return; }
+                    MessageBox.Show("Данные успешно зашифрованы", "Info", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string dataFileName;
+            string keyFileName;
+
+            MessageBox.Show("Выберете файл для дешифрования", "", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                dataFileName = openFileDialog1.FileName;
+            } else { return; }
+
+            MessageBox.Show("Выберете файл ключа", "", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                keyFileName = openFileDialog1.FileName;
+            } else { return; }
+
+            //дешифрование
+            string decrypted = CipherRe.Decrypt(dataFileName, keyFileName);
+
+            MessageBox.Show("Дешифрование выполнено. Сохраните файл с расшифрованными данными", "Save", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files(*.txt)|*.txt";
+            DialogResult result1 = saveFileDialog.ShowDialog();
+            if (result1 == DialogResult.OK)
+            {
+                string filename = saveFileDialog.FileName;
+                File.WriteAllText(filename, decrypted);
+            } else { return; }
+            MessageBox.Show("Данные успешно расшифрованы", "Info", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+}
